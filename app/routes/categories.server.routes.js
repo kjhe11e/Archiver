fd51e7e800f9c980d67375de413b8e4d50bd59af
Kyle Helle <kjhelle4@gmail.com>
2017-01-16 21:29:52 -0800

    Add authentication

--- app/routes/categories.server.routes.js
+++ app/routes/categories.server.routes.js
@@ -3,15 +3,16 @@
 module.exports = function(app) {
 	// Routing logic   
 	var categories = require('../../app/controllers/categories.server.controller');
+	var users = require('../../app/controllers/users.server.controller');
 
 	app.route('/categories')
 		.get(categories.list)
-		.post(categories.create);
+		.post(users.requiresLogin, categories.create);
 
 	app.route('/categories/:categoryId')
 		.get(categories.read)
-		.put(categories.update)
-		.delete(categories.delete);
+		.put(users.requiresLogin, categories.update)
+		.delete(users.requiresLogin, categories.delete);
 
 	app.param('categoryId', categories.categoryById);
 };
